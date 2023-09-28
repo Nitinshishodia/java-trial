0087c043f4852dc044c0993386b4cc4179026e32
Nitinshishodia <nitinshishodia123@gmail.com>
2023-09-28 09:37:26 +0530

    First commit

---
+++ pw.java/pw.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+public class SumDigitsRecrsion {
+    public static int sumDigit(int num){
+    if(num==0){
+        return 0;
+    }
+    else{
+        return num%10 +sumDigit(num/10);
+    }
+}
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=Sc.nextInt();
+        //System.out.println("the sum of digit is:"+ SumDigit(n));
+        int sum=sumDigit(num);
+        System.out.println("Sum of digit is:"+sum);
+    }
+}
